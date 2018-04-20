831d5a46415dfbd5298386f011456eca39a1ac0e
TTeun <teunverstraaten@gmail.com>
2018-04-20 15:35:44 +0200

    Adsadas

--- lab1/funcs_puls.py
+++ lab1/funcs_puls.py
@@ -2,18 +2,36 @@ import numpy as np
 import math
 
 
-def S(k,x,L):
-	return np.sin(math.pi* (2*k+1)* x/L)
+def s_k(k,x,L):
+	x[:] = [x_i * math.pi * (2. * k + 1.) / L for x_i in x]
+	return np.sin(x)
 
-def Psi(k,t,sigma,omega):
-	return (2*k+1)**2*sigma*math.pi**2*np.sin(omega*t) - omega*np.cos(omega*t) + omega*np.exp(-1*(2*k+1)**2*sigma*math.pi**2*t)
+def psi_k(k,t,sigma,omega):
+	c1 = (2 * k + 1) ** 2
+	result = c1 * sigma * math.pi ** 2 * np.sin(omega * t)
+	result -= omega * np.cos(omega * t) 
+	result += omega * np.exp(-1 * c1 * sigma * math.pi ** 2*t)
+	return  result
 
 def sigma(mu, rho, L):
 	return float(mu) / (rho * L * L)
 
 def d_k(k, rho, sigma, omega):
 	return 4. / (rho * math.pi * (2. * k + 1.)  * ( (2. * k + 1. ) ** 4 * sigma ** 2 * math.pi ** 4 + omega ** 2))
-	
+
+def transient(t, max_k, mu = 0.035, rho = 1, L = 1, a = 1, omega = 1):
+	s = sigma(mu, rho, L)
+	x = list(np.linspace(0, L))
+	u = [0.] * len(x)
+	for k in range(max_k):
+		u += a * d_k(k, rho, s, omega) * psi_k(k, t, s, omega) * s_k(k, x, L)
+	return u
+
+
+print (transient(1, 15) - transient(1, 20))
+
+
+
 def cc(x):
 	return np.cos(x) * np.cosh(x)
 
