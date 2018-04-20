638011cdfb24494685266b0d2f4f686970ee89f4
TTeun <teunverstraaten@gmail.com>
2018-04-20 15:18:59 +0200

    sigma en d_k

--- lab1/funcs_puls.py
+++ lab1/funcs_puls.py
@@ -1,5 +1,13 @@
 import numpy as np
+import math
+		
 
+def sigma(mu, rho, L):
+	return float(mu) / (rho * L * L)
+
+def d_k(k, rho, sigma, omega):
+	return 4. / (rho * math.pi * (2. * k + 1.)  * ( (2. * k + 1. ) ** 4 * sigma ** 2 * math.pi ** 4 + omega ** 2))
+	
 def cc(x):
 	return np.cos(x) * np.cosh(x)
 
