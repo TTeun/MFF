0f5a0924427975c9f14e6b67ef32a3724cc7cead
TTeun <teunverstraaten@gmail.com>
2018-05-01 21:44:49 +0200

    Asdsad

--- lab2/2_2.py
+++ lab2/2_2.py
@@ -9,69 +9,56 @@ set_log_level(ERROR)
 def diffreac(N,  bc_type='dirichlet'):
 	# Create mesh and define function space
 	mesh = UnitSquareMesh(N, N)
-	V = FunctionSpace(mesh, "Lagrange", 2)
-	u0 = Expression("1 + 2*x[0]*x[0] + 3*x[1]*x[1]", degree=2)
+	V = FunctionSpace(mesh, "Lagrange", 1)
+	u0 = Expression("9 + 2*x[0]*x[0] + 3*x[1]*x[1]", degree=2)
 
 	u = TrialFunction(V)
 	v = TestFunction(V)
 	f = Expression("-10", degree=2) + u0
 	L = f*v*dx
-	boundaries = FacetFunction('size_t', mesh)
 
 	if (bc_type == 'dirichlet'):
+		boundaries = FacetFunction('size_t', mesh)
 		bc = DirichletBC(V, u0, boundaries, 0)
 
 		a = (u * v) * dx + inner(grad(u), grad(v))*dx
 		u = Function(V)
 
 		solve(a == L, u, bc)
+	else:
+		if (bc_type == 'neumann'):
+			[boundaries, ds] = set_neumann_robin_boundary(mesh)
+			u_x = Expression('4 * x[0]', degree=1)
+			u_y = Expression('6 * x[1]', degree=1)
 
-	if (bc_type == 'neumann'):
-		T_v, T_h = [T_VERT(), T_HORI()]
-		T_v.mark(boundaries, 0)
-		T_h.mark(boundaries, 1)
- 		ds = Measure('ds', domain=mesh, subdomain_data=boundaries)
+			a = (u * v) * dx + inner(grad(u), grad(v))*dx
+			u = Function(V)
+			L += u_y * v * ds(1) +  u_x * v * ds(0) 
 
-		u_x = Expression('4 * x[0]', degree=1)
-		u_y = Expression('6 * x[1]', degree=1)
+			solve(a == L, u)
 
-		a = (u * v) * dx + inner(grad(u), grad(v))*dx
-		u = Function(V)
-		L += u_y * v * ds(1) +  u_x * v * ds(0) 
+		if (bc_type == 'robin'):
+			[boundaries, ds] = set_neumann_robin_boundary(mesh)
+			u_x = Expression('4 * x[0]', degree=1) + u0
+			u_y = Expression('6 * x[1]', degree=1) + u0
 
-		solve(a == L, u)
+			a = (u * v) * dx + inner(grad(u), grad(v))*dx - (u * v) * ds
+			u = Function(V)
+			L += u_y * v * ds(1) +  u_x * v * ds(0) 
 
-	if (bc_type == 'robin'):
-		T_v, T_h = [T_VERT(), T_HORI()]
-		T_v.mark(boundaries, 0)
-		T_h.mark(boundaries, 1)
- 		ds = Measure('ds', domain=mesh, subdomain_data=boundaries)
+			solve(a == L, u)
 
-		u_x = Expression('4 * x[0]', degree=1) + u0
-		u_y = Expression('6 * x[1]', degree=1) + u0
+		if (bc_type == 'mixed'):
+			[boundaries, ds] = set_mixed_boundary(mesh)
+			u_right = Expression('4', degree=1)
+			u_y = Expression('6 * x[1]', degree=1) + u0
+			bc = DirichletBC(V, u0, boundaries, 0)
 
-		a = (u * v) * dx + inner(grad(u), grad(v))*dx - (u * v) * ds
-		u = Function(V)
-		L += u_y * v * ds(1) +  u_x * v * ds(0) 
+			a = (u * v) * dx + inner(grad(u), grad(v))*dx - (u * v) * ds(2)
+			u = Function(V)
+			L += u_right * v * ds(1) + u_y * v * ds(2)
 
-		solve(a == L, u)
-
-	if (bc_type == 'mixed'):
-		T_left, T_right, T_hori = [T_LEFT(), T_RIGHT(), T_HORI()]
-		T_left.mark(boundaries, 0)
-		T_right.mark(boundaries, 1)
-		T_hori.mark(boundaries, 2)
- 		ds = Measure('ds', domain=mesh, subdomain_data=boundaries)
-		
-		u_right = Expression('4', degree=1)
-		u_y = Expression('6 * x[1]', degree=1) + u0
-		bc = DirichletBC(V, u0, boundaries, 0)
-
-		a = (u * v) * dx + inner(grad(u), grad(v))*dx - (u * v) * ds(2)
-		u = Function(V)
-		L += u_right * v * ds(1) + u_y * v * ds(2)
-
-		solve(a == L, u, bc)
+			solve(a == L, u, bc)
 
 	# Plot solution
 	plot(u, interactive=True)
--- lab2/domains.py
+++ lab2/domains.py
@@ -23,3 +23,24 @@ class T_LEFT(SubDomain):
 class T_RIGHT(SubDomain):
     def inside(self, x, on_boundary):
         return on_boundary and (near(x[0], 1.0))
+
+def set_neumann_robin_boundary(mesh):
+    boundaries = FacetFunction('size_t', mesh)
+    T_v, T_h = [T_VERT(), T_HORI()]
+    T_v.mark(boundaries, 0)
+    T_h.mark(boundaries, 1)
+
+    ds = Measure('ds', domain=mesh, subdomain_data=boundaries)    
+    
+    return [boundaries, ds]
+
+def set_mixed_boundary(mesh):
+    boundaries = FacetFunction('size_t', mesh)
+    T_left, T_right, T_hori = [T_LEFT(), T_RIGHT(), T_HORI()]
+    T_left.mark(boundaries, 0)
+    T_right.mark(boundaries, 1)
+    T_hori.mark(boundaries, 2)    
+
+    ds = Measure('ds', domain=mesh, subdomain_data=boundaries)    
+    
+    return [boundaries, ds]
