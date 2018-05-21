4ef2bfa6f5bfad7e4ee00c57a0154be50144ca87
Soksoksok <leo.sok@gmail.com>
2018-05-21 19:37:26 +0200

    afds

--- lab4/2_1.py
+++ lab4/2_1.py
@@ -4,9 +4,11 @@ import numpy as np
 
 def stokes(p2p1 = True, epsi = 1):
 	'''
-	method = 'p2p1' or 'p1p1stab'
+	p2p1 = True for p2p1 elements and
+		   False for p1p1 stabilized elements
 	'''
 
+	# Define domains
 	class T_LEFT(SubDomain):
 		def inside(self, x, on_boundary):
 			return on_boundary and (near(x[0], 0.0))
@@ -25,13 +27,17 @@ def stokes(p2p1 = True, epsi = 1):
 
         
         
+	# Some constants
 	Len = 6.
 	mu = 0.001
 	ny = 32
 	nx = 3*ny
 	ny = ny/2
+	
+	# Create mesh
 	mesh = RectangleMesh(Point(0,0), Point(Len,1), nx, ny)
 	
+	# define elements and functionspace
 	if p2p1:
 		VE = VectorElement('P', mesh.ufl_cell(), 2)
 	else:
@@ -42,13 +48,16 @@ def stokes(p2p1 = True, epsi = 1):
 
 	W = FunctionSpace(mesh, ME)
 
+	# Define functions
 	u, p = TrialFunctions(W)
 	v, q = TestFunctions(W)
 	
-	a = mu * inner(grad(u), grad(v))*dx + div(u)*q*dx - div(v)*p*dx 
+	# Set up the problem
+	a = mu * inner(grad(u), grad(v)) * dx + div(u) * q * dx - div(v) * p * dx 
 
 	if not p2p1:
-		h = 0.01 #CellDiameter(mesh)
+		# Add stabilisation term
+		h = 0.01 #CellDiameter(mesh) # CellDiameter does not work on my laptop
 		stab = epsi * (h * h / mu) * inner(grad(p),grad(q)) * dx
 		a += stab
 		
@@ -64,36 +73,34 @@ def stokes(p2p1 = True, epsi = 1):
 	T_b.mark(boundaries, 4)
 	ds = Measure('ds', domain=mesh, subdomain_data=boundaries)
 
+	# Add Dirichlet bc's
 	bcs = []
 	bc = DirichletBC(W.sub(0), [0,0], boundaries, 3)
 	bcs.append(bc)
 	bc = DirichletBC(W.sub(0).sub(1), 0, boundaries, 4)
 	bcs.append(bc)
 
+	# Add Neumann bc
 	n = Constant([1,0])
 	L += inner(v, n) * ds(1)
 
+	# Solve the problem
 	w = Function(W)
-
 	solve(a == L, w, bcs)
 
+	# Split up the solution
 	u, p = w.split()  
-
+	
 	File('sol.pvd') << u
 
-
-
-
-
-	# Just a figure and one subplot
+	# Plot both u and p in one figure
 	fig = plt.figure()
-
+	
 	plt.subplot(2, 1, 1)
 	c = plot(u)
 	plt.title('results')
 	plt.colorbar(c)
 	plt.ylabel('y')
-
 
 	plt.subplot(2, 1, 2)
 	d = plot(p)
