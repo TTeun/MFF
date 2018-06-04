ad5cc9cc241bef5db655e32ceeba0ebb0e2bf297
TTeun <teunverstraaten@gmail.com>
2018-06-04 21:08:16 +0200

    asd

--- lab5/2.py
+++ lab5/2.py
@@ -2,6 +2,8 @@ from dolfin import *
 import matplotlib.pyplot as plt
 import numpy as np
 
+Len = 6.
+
 # Define domains
 class T_LEFT(SubDomain):
 	def inside(self, x, on_boundary):
@@ -19,68 +21,66 @@ class T_BOTTOM(SubDomain):
 	def inside(self, x, on_boundary):
 		return on_boundary and (near(x[1], 0.0))
 
-def func(t):
-	f = 0.
-	for j in range(1,4):
-		f += (4000./j) * np.sin(j * 2 * np.pi * t)
-	return f
+def transient_stokes(deltat):
+	# Some constants
+	mu = 0.035
+	ny = 16
+	nx = 6*ny
+	rho = 1.2
+	Tf = 2
+	theta = 1.0
 
-# Some constants
-Len = 6.
-mu = 0.035
-ny = 32
-nx = 3*ny
-ny = ny/2
-rho = 1.2
-Tf = 2
-deltat = 0.1
-theta = 0.5
+	# Neumann condition for x = 0
+	f = Expression("4000. * sin(2 * pi * t) + 2000. * sin(4 * pi * t) + 1333.3 * sin(6 * pi * t)", t=0, degree=3)
 
-# Create mesh
-mesh = RectangleMesh(Point(0,0), Point(Len,1), nx, ny)
+	# Create mesh
+	mesh = RectangleMesh(Point(0,0), Point(Len,1), nx, ny)
 
-# define elements and functionspace
-VE = FiniteElement('P', mesh.ufl_cell(), 2)
+	# define elements and functionspace
+	VE = FiniteElement('P', mesh.ufl_cell(), 2)
 
-W = FunctionSpace(mesh, VE)
+	W = FunctionSpace(mesh, VE)
 
-# Define functions
-u = TrialFunction(W)
-v = TestFunction(W)
+	# Define functions
+	u = TrialFunction(W)
+	v = TestFunction(W)
 
-# Split up the boundary
-boundaries = FacetFunction('size_t', mesh)
-T_l, T_r, T_t, T_b = [T_LEFT(), T_RIGHT(), T_TOP(), T_BOTTOM()]
-T_l.mark(boundaries, 1)
-T_r.mark(boundaries, 2)
-T_t.mark(boundaries, 3)
-T_b.mark(boundaries, 4)
-ds = Measure('ds', domain=mesh, subdomain_data=boundaries)
+	# Split up the boundary
+	boundaries = FacetFunction('size_t', mesh)
+	T_l, T_r, T_t, T_b = [T_LEFT(), T_RIGHT(), T_TOP(), T_BOTTOM()]
+	T_l.mark(boundaries, 1)
+	T_r.mark(boundaries, 2)
+	T_t.mark(boundaries, 3)
+	T_b.mark(boundaries, 4)
+	ds = Measure('ds', domain=mesh, subdomain_data=boundaries)
 
-# Set up the problem
-a = rho * u * v * dx + deltat * theta * mu * inner(grad(u), grad(v)) * dx 
+	# Set up the problem
+	a = rho * u * v * dx + deltat * theta * mu * inner(grad(u), grad(v)) * dx 
 
-u0 = interpolate(Constant(0), W)
+	u0 = interpolate(Constant(0), W)
 
-# Add Dirichlet bc's
-bcs = []
-bc = DirichletBC(W, 0, boundaries, 3)
-bcs.append(bc)
+	# Add Dirichlet bc's
+	bcs = []
+	bc  = DirichletBC(W, 0, boundaries, 3)
+	bcs.append(bc)
 
-A = assemble(a)
-[bc.apply(A) for bc in bcs]
+	A = assemble(a)
+	[bc.apply(A) for bc in bcs]
 
-sol = XDMFFile('u.xdmf')
-sol.parameters['rewrite_function_mesh'] = False
+	sol = XDMFFile('u.xdmf')
+	sol.parameters['rewrite_function_mesh'] = False
 
-for t in np.arange(0.0, Tf, deltat):
-	n = Constant([1,0])
-	L = rho * u0 * v * dx - deltat * (1 - theta) * mu * inner(grad(u0), grad(v)) * dx
-	L += deltat * func(t + theta * deltat) / Len * v * dx
+	for t in np.arange(deltat, Tf + deltat, deltat):
+		n = Constant([1,0])
+		L = rho * u0 * v * dx - deltat * theta * mu * inner(grad(u0), grad(v)) * dx
+		f.t = t + theta * deltat
+		L += deltat * f / Len * v * dx
 
-	b = assemble(L)
-	u1 = Function(W, name='solution')
-	solve(A, u1.vector(), b)
-	sol.write(u1, t)
-	
-	u0.assign(u1)
+		b = assemble(L)
+		u1 = Function(W, name='solution')
+		solve(A, u1.vector(), b)
+		sol.write(u1, t)
+		
+		u0.assign(u1)
+
+transient_stokes(0.05)
